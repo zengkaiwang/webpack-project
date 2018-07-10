ccc68e0a77a632bc094e5b387f3581eac1036f79
王增凯 <zengkai.wang@irootech.com>
2018-07-10 17:15:12 +0800

    vue-loader编译报错解决

---
+++ webpack2_vue2/src/app.vue
@@ -0,0 +1,15 @@
+<template> 
+		<div class="app"> 
+			{{msg}} 
+		</div> 
+</template>
+
+ <script> 
+ 		export default { 
+ 			data(){ 
+ 				return { msg:'erererer' } 
+ 			} 
+ 		} 
+ </script> 
+
+
---
+++ webpack2_vue2/webpack.config.js
@@ -0,0 +1,31 @@
+/* 引入操作路径模块和webpack */
+var path = require('path');
+var webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = { 
+	entry: './src/main.js', 
+
+	output: { 
+		path: path.resolve(__dirname, './dist'), 
+		publicPath: '/dist/', 
+		filename: 'build.js' 
+	}, 
+  plugins: [
+      // make sure to include the plugin for the magic
+      new VueLoaderPlugin()
+  ],	
+	module: { 
+		rules: [ 
+			{ 
+				test: /\.vue$/, 
+				loader: 'vue-loader' 
+			}, 
+			{ 
+				test: /\.js$/, 
+				loader: 'babel-loader', 
+				exclude: /node_modules/ 
+			}
+		] 
+	} 
+}

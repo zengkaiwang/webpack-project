e201dec8c439be3bbaa65d9bf09a30614e8d2d80
王增凯 <zengkai.wang@irootech.com>
2018-07-11 10:09:49 +0800

    编译.vue文件中css报错解决

--- webpack2_vue2/src/app.vue
+++ webpack2_vue2/src/app.vue
@@ -1,15 +1,22 @@
 <template> 
 		<div class="app"> 
-			{{msg}} 
+			{{msg}} z	
 		</div> 
 </template>
 
  <script> 
  		export default { 
  			data(){ 
- 				return { msg:'erererer' } 
+ 				return { msg:'aaaaaaa' } 
  			} 
  		} 
  </script> 
 
+ <style ;> 
+ 		.app { 
+ 			width: 100%; 
+ 			height: 50px; 
+ 			border: 1px solid red; 
+ 		} 
+ </style>
 
